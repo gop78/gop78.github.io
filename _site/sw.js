9b2b722769ab5816723138e9c8dfb22570884344
gop78@naver.com <gop78@naver.com>
2022-03-17 01:10:00 +0900

    테마 적용3

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220316.2359';
+const cacheName = 'chirpy-20220317.0000';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
